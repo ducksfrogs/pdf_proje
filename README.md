cd3367d20413deab28f445b16e4e08eb5e55ad87
ducksfrogs <ma_yamaki@yahoo.com>
2022-08-27 13:36:24 +0900

    test fin

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ This project is making many pdf files join by 2 for Mashiko Housei high school.
 
 Day1(22/08/26)
 making dummy pdf files. importing libraries pypdf2, glob.
+
+run on notebook.
--- code/pdf_add.py
+++ code/pdf_add.py
@@ -1,1 +1,19 @@
 import glob
+import shutil
+import PyPDF2 as pd
+
+arr = glob.glob('../src/*.pdf')
+arr.sort
+
+it = iter(arr)
+nitr = int(len(arr)/2)
+
+for i, j, k in zip(range(nitr), it, it):
+    merger = pd.PdfMerger()
+    merger.append(j)
+    merger.append(k)
+    merger.write(f'test{i}.pdf')
+    merger.close()
+    shutil.move(f'test{i}.pdf', '../dist/')
+
+print("Done making pdf files")
